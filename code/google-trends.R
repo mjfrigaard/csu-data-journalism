b915a1ab6bb53e5aead707b79cc54f6468c837af
Martin J Frigaard <mjfrigaard@gmail.com>
2021-08-20 18:38:18 -0700

    updates

---
+++ code/google-trends.R
@@ -0,0 +1,12 @@
+
+devtools::install_github('PMassicotte/gtrendsR')
+
+library(gtrendsR)
+library(maps)
+library(ggplot2)
+library(lettercase)
+library(viridis)
+library(pals)
+library(scico)
+library(ggrepel)
+library(tidyverse)
